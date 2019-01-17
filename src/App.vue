a807bfff3538ad61b02d973e849ed5917050130a
Justine <justinecomment@gmail.com>
2019-01-17 13:08:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/img/header.jpg" />
+    <h2>Bibliotheque</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  body{
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+
+  }
+
+  img{
+    width: 100%;
+  }
+
+  h2{
+    font-family: Calibri , impact;
+  }
+
+</style>
